b2806b22d04ef787834a81a18dd062f9cf9b641c
berna.delikanli <berna.delikanli@BERNA-NB>
2021-01-23 17:35:16 +0300

    Add project files.

---
+++ OdevDictonary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdevDictonary
+{
+    class MyDictionary<A,P>
+    {
+        A[] Ad;
+        P[] Puan;
+        public MyDictionary()   //ctor 
+        {
+            Ad = new A[0];
+            Puan = new P[0];
+        }
+
+
+        public void Add(A Ads, P Puans)
+        {
+            A[] tempAd = Ad;
+            P[] tempPuan= Puan;
+            Ad = new A[Ad.Length + 1];
+            Puan = new P[Puan.Length + 1];
+
+            for (int i = 0; i < tempAd.Length; i++)
+            {
+                Ad[i] = tempAd[i];
+                Puan[i] = tempPuan[i];
+            }
+
+            Ad[Ad.Length - 1] = Ads;
+            Puan[Puan.Length - 1] = Puans;
+        }
+
+
+    }
+}
---
+++ OdevDictonary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OdevDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> AdPuan = new Dictionary<string, int> ();
+           // Console.WriteLine(AdPuan[0]);
+            AdPuan.Add("Sağre", 58);
+            AdPuan.Add("Furkan", 89);
+            AdPuan.Add("Gül", 96);
+            AdPuan.Add("Kemal", 78);
+            AdPuan.Add("Veli", 98);
+            AdPuan.Add("Can", 18);
+            AdPuan.Add("Güler", 15);
+            Console.WriteLine(AdPuan.Count);
+        }
+    }
+}
